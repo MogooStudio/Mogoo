839ff093e8c71fcc3e8c29d82a7122bf21127022
MogooStudio <mogoostudio@outlook.com>
2020-04-21 02:33:44 +0800

    mono扩展

---
+++ Assets/Scripts/Mogoo/Mono/MonoManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mogoo.Base;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Internal;
+
+namespace Mogoo.Mono
+{
+    public class MonoManager : Singleton<MonoManager>
+    {
+        private MonoController _controller;
+
+        public MonoManager()
+        {
+            var gameObject = new GameObject("MonoController");
+            _controller = gameObject.AddComponent<MonoController>();
+        }
+
+        public void AddUpdateListener(UnityAction func)
+        {
+            _controller.AddUpdateListener(func);
+        }
+
+        public void RemoveUpdateListener(UnityAction func)
+        {
+            _controller.RemoveUpdateListener(func);
+        }
+
+        public Coroutine StartCoroutine(IEnumerator routine)
+        {
+            return _controller.StartCoroutine(routine);
+        }
+
+        public Coroutine StartCoroutine(string methodName)
+        {
+            return _controller.StartCoroutine(methodName);
+        }
+
+        public Coroutine StartCoroutine(string methodName, [DefaultValue("null")] object value)
+        {
+            return _controller.StartCoroutine(methodName, value);
+        }
+
+    }
+}
+
---
+++ Assets/Scripts/Test/mono/monoTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mogoo.Mono;
+using UnityEngine;
+
+public class TestUpdate
+{
+    public TestUpdate()
+    {
+        MonoManager.Instance.StartCoroutine(print1());
+    }
+
+    public void print()
+    {
+        Debug.Log("111111");
+    }
+
+    IEnumerator print1()
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log("22222");
+    }
+}
+
+public class monoTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestUpdate test = new TestUpdate();
+        MonoManager.Instance.AddUpdateListener(test.print);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
