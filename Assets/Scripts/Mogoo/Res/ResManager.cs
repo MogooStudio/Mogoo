77a2bcc9adbd319c8f66277a510225f20f50820d
MogooStudio <mogoostudio@outlook.com>
2020-04-22 23:28:11 +0800

    资源加载模块

---
+++ Assets/Scripts/Mogoo/Res/ResManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mogoo.Base;
+using Mogoo.Mono;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ResManager : Singleton<ResManager>
+{
+    public T Load<T>(string path) where T:Object
+    {
+        T t = Resources.Load<T>(path);
+        if (t is GameObject)
+        {
+            return GameObject.Instantiate(t);
+        }
+        else
+        {
+            return t;
+        }
+    }
+
+    public void LoadAsync<T>(string path, UnityAction<T> func) where T:Object
+    {
+        MonoManager.Instance.StartCoroutine(loadAsync(path, func));
+    }
+
+    private IEnumerator loadAsync<T>(string path, UnityAction<T> func) where T : Object
+    {
+        ResourceRequest r = Resources.LoadAsync(path);
+        yield return r;
+        if (r.asset is GameObject)
+        {
+            func(GameObject.Instantiate(r.asset) as T);
+        }
+        else
+        {
+            func(r.asset as T);
+        }
+    }
+}
---
+++ Assets/Scripts/Test/res/resTest.cs
@@ -0,0 +1,28 @@
+﻿using Mogoo.Pool;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resTest : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ResManager.Instance.Load<GameObject>("Test/Cube");
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            ResManager.Instance.LoadAsync<GameObject>("Test/Cube", (GameObject obj) =>
+            {
+                Debug.Log("GetMouseButtonDown 1");
+            });
+        }
+    }
+}
